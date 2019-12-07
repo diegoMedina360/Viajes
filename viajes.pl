59f9ac43b95c0b39affc15a0677b7c0055f82fe9
Diego <diego1998geovanny@gmail.com>
2019-12-07 14:09:08 -0500

    Update viajes.pl

--- viajes.pl
+++ viajes.pl
@@ -114,15 +114,13 @@ viaje2(C1,C2,[Int1,Int2]) :-
                      camino2(C2,C1,_,_);
                      viaje2(C1,Int1,[]),viaje2(Int1,C2,Int2).
 
-viaje3(C1,C2,[],T) :- camino2(C1,C2,_,_),obtenerTiempo(C1,C2,Ti1),Ti11 is Ti1,obtenerTiempo(C1,C2,Ti2),Ti22 is Ti2,
-                   (medioTiempo(1,0,(Ti11),(Ti22),TiemR)),T is TiemR;
-                   camino2(C2,C1,_,_),obtenerTiempo(C2,C1,Ti1),Ti11 is Ti1,obtenerTiempo(C2,C1,Ti2),Ti22 is Ti2,
-                   (medioTiempo(1,0,(Ti11),(Ti22),TiemR)),T is TiemR.
-viaje3(C1,C2,[Int1,Int2],T) :-camino2(C1,C2,_,_),obtenerTiempo(C1,C2,Ti1),Ti11 is Ti1,obtenerTiempo(C1,C2,Ti2),Ti22 is Ti2,
-                   (medioTiempo(1,0,(Ti11),(Ti22),TiemR)),T is TiemR;
-                   camino2(C2,C1,_,_),obtenerTiempo(C2,C1,Ti1),Ti11 is Ti1,obtenerTiempo(C2,C1,Ti2),Ti22 is Ti2,
-                   (medioTiempo(1,0,(Ti11),(Ti22),TiemR)),T is TiemR;
-                   viaje3(C1,Int1,[],T1),viaje3(Int1,C2,Int2,T2),T is T1+T2.                     
+viaje3(C1,C2,[],T,D) :- camino2(C1,C2,_,_),viajeIda(C1,C2,Ti,Di),T is Ti,D is Di;
+                   camino2(C2,C1,_,_),viajeRegreso(C1,C2,Ti,Di),T is Ti,D is Di.
+
+viaje3(C1,C2,[Int1,Int2],T,D) :-camino2(C1,C2,_,_),viajeIda(C1,C2,Ti,Di),T is Ti,D is Di;
+                   camino2(C2,C1,_,_),viajeRegreso(C1,C2,Ti,Di),T is Ti,D is Di;
+                   viaje3(C1,Int1,[],T1,D1),viaje3(Int1,C2,Int2,T2,D2),T is T1+T2,D is D1+D2;
+                   viaje3(C2,Int1,[],T1,D1),viaje3(Int1,C1,Int2,T2,D2),T is T1+T2,D is D1+D2.                     
 
 getFirElem([X|_],X).
 
